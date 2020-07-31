2d6c6793df6302c402face625b2d045f477553e5
kuninari <johoiu98@gmail.com>
2020-07-31 13:53:08 +0900

    Fix: statusの値をシンボルに修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     sequence(:title, 'title_1')
     content { 'コンテンツ' }
-    status { 0 }
+    status { :todo }
     # factoryで定義している名称がモデル名と一致する場合、associationを省略できる。
     user
   end
