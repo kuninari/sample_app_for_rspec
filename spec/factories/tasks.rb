6a91f2ea7463041c615546b93533cc0ee04fc0df
kuninari <johoiu98@gmail.com>
2020-07-31 13:44:27 +0900

    Fix: factoryをシーケンスに修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :task do
-    title { 'タイトル' }
+    sequence(:title, 'title_1')
     content { 'コンテンツ' }
     status { 0 }
     # factoryで定義している名称がモデル名と一致する場合、associationを省略できる。
