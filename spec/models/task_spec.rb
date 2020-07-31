475609a06e7d051c3c655a99d7ec349923adfe07
kuninari <johoiu98@gmail.com>
2020-07-31 15:53:53 +0900

    Fix: describeの削除

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,38 +4,34 @@ RSpec.describe Task, type: :model do
   describe "バリデーションテスト" do
     let (:task) { build(:task) }
 
-    describe "バリデーションが正しい場合" do
-      context "タイトルとステータスがある場合" do
-        it "有効であること" do
-          expect(task).to be_valid
-          expect(task.errors).to be_empty
-        end
+    context "タイトルとステータスがある場合" do
+      it "有効であること" do
+        expect(task).to be_valid
+        expect(task.errors).to be_empty
       end
     end
 
-    describe "バリデーションが正しくない場合" do
-      context "タイトルが空の場合" do
-        it "無効であること" do
-          task.title = ""
-          expect(task).to be_invalid
-          expect(task.errors[:title]).to eq ["can't be blank"]
-        end
+    context "タイトルが空の場合" do
+      it "無効であること" do
+        task.title = ""
+        expect(task).to be_invalid
+        expect(task.errors[:title]).to eq ["can't be blank"]
       end
+    end
 
-      context "ステータスが空の場合" do
-        it "無効であること" do
-          task.status = ""
-          expect(task).to be_invalid
-          expect(task.errors[:status]).to eq ["can't be blank"]
-        end
+    context "ステータスが空の場合" do
+      it "無効であること" do
+        task.status = ""
+        expect(task).to be_invalid
+        expect(task.errors[:status]).to eq ["can't be blank"]
       end
+    end
 
-      context "タイトルが重複している場合" do
-        it "無効であること" do
-          task_with_duplicated_title = build(:task, title: create(:task).title)
-          expect(task_with_duplicated_title).to be_invalid
-          expect(task_with_duplicated_title.errors[:title]).to eq ["has already been taken"]
-        end
+    context "タイトルが重複している場合" do
+      it "無効であること" do
+        task_with_duplicated_title = build(:task, title: create(:task).title)
+        expect(task_with_duplicated_title).to be_invalid
+        expect(task_with_duplicated_title.errors[:title]).to eq ["has already been taken"]
       end
     end
   end
