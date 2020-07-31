8aa6e3b861ffcf4c6cd087305a57ee4134d7ebe9
kuninari <johoiu98@gmail.com>
2020-07-31 13:56:11 +0900

    Add: factoryにdeadlineを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:title, 'title_1')
     content { 'コンテンツ' }
     status { :todo }
+    deadline { 1.week.from_now }
     # factoryで定義している名称がモデル名と一致する場合、associationを省略できる。
     user
   end
