a0c7c913c713ff0299e7d3eee65db6fcae60e372
kuninari <johoiu98@gmail.com>
2020-08-07 23:34:07 +0900

    Fix: タスクの一覧ページに遷移した際のテストスイートを修正

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -29,9 +29,12 @@ RSpec.describe "Tasks", type: :system do
       end
     end
       context 'タスクの一覧ページに遷移' do
-        it '一覧ページに遷移できる' do
+        it 'すべてのユーザーのタスク情報が表示される' do
+          task_list = create_list(:task, 3)
           visit tasks_path
-          expect(page).to have_content 'Tasks'
+          expect(page).to have_content task_list[0].title
+          expect(page).to have_content task_list[1].title
+          expect(page).to have_content task_list[2].title
           expect(current_path).to eq tasks_path
         end
       end
